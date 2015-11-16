6eb98e2035a80235040e2f685fee9921e07e670e
milinkorath <milinkorath@gmail.com>
2015-11-16 09:47:37 +0530

    Inital project commit

---
+++ WebContent/js/app/app.js
@@ -0,0 +1,27 @@
+var app=angular.module("app1",[]);
+app.config(function($routeProvider)
+{
+	$routeProvider.when("/login", {
+		templateUrl: "login.html",
+		controller: "LoginController"
+		
+	});
+	
+	$routeProvider.when("/home", {
+		templateUrl: "home.html",
+		controller: "HomeController"
+		
+	});
+	
+	$routeProvider.otherwise({redirectTo: "/login"})
+
+});
+
+app.controller("LoginController", function($scope) {
+	
+	$scope.title="MyTitle";
+})
+app.controller("HomeController", function($scope) {
+	
+	$scope.title="MyTitle";
+})
